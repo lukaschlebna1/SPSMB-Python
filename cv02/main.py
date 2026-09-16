ff62cf7e6c261a333493e6cc7e5795d6092f7126
Lukáš Chlebna <lukas.chlebna@student.spsmb.cz>
2026-09-16 15:28:49 +0200

    add cv02

--- cv02/main.py
+++ cv02/main.py
@@ -1,3 +1,4 @@
+
 from random import random
 
 def get_color():
@@ -13,33 +14,39 @@ def start_game():
     user_bilance = 1_000
     print("Welcome to casino Royal")
     while True:
-        bet = int(input(f"Select your bet ({user_bilance}eur):"))
-
+        bet = int(input(f"Select your bet ({user_bilance}eur): "))
+        while bet > user_bilance or bet <= 0:
+            if bet <= 0:
+                print("Sázka musí být větší než 0.")
+            else:
+                print("Dej min (nemáš dost peněz)")
+            bet = int(input(f"Select your bet ({user_bilance}eur): "))
+       
         print("Select color:")
         print("\t\t0 - Red")   # 48.5%
         print("\t\t1 - White") # 48.5%
         print("\t\t2 - Green") # 3%
-        print("\t\t9 - leave game")
-        
+        print("\t\t4 - leave game")
+       
         selection = int(input("Select: "))
+       
+        while selection > 4 or selection < 0:
+            print("Neplatná volba, zkus to znovu.")
+            selection = int(input("Select: "))
 
-        if selection == 9:
+        if selection == 4:
             return
-        
+       
         if selection == get_color():
-            user_bilance = user_bilance + bet * 2
+            user_bilance += bet
             print("You won!!")
         else:
+            user_bilance -= bet
             print(f"You lost {bet}eur")
-            user_bilance = user_bilance - bet
+
+        if user_bilance <= 0:
+            print("Došly ti peníze! Konec hry.")
+            break
 
 if __name__ == "__main__":
     start_game()
-    total_count = 0
-    color = 0
-    for _ in range(1_000_000):
-        if get_color() == 0:
-            color = color + 1
-        total_count = total_count + 1
-
-    print(color / total_count * 100)
